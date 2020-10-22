fa20cb265fa6f40f1cda1c739dd4706838039369
Farcy Corentin <corentinfarcy1@gmail.com>
2020-10-22 16:53:40 +0200

    Fil rouge

---
+++ application/views/header.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap.min"); ?>">
+    <link rel="stylesheet" href="<?php echo base_url("assets/css/style.css"); ?>">
+    <title>Village green</title>
+</head>
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <div col-3>
+                <img src="<?php echo base_url("assets/image/HEADER/3_bandes.png"); ?> " id="bg" classe="img-fluid" alt="">
+            </div>
+            <div col-9>
+                <img src="<?php echo base_url("assets/image/HEADER/logo_village_green.png"); ?>" id="logo" classe="img-fluid" alt="">
+            </div> 
+        </div>
+        <ul class="nav ">
+  <li class="nav-item">
+    <a class="nav-link active" href="#">Info</a>
+  </li>
+  <li class="nav-item">
+    <a class="nav-link" href="#">Espace Client</a>
+  </li>
+</ul>
+        
+    
